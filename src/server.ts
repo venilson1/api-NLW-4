e9737aa9caecfcd08b6b0f850967ca942f74d2ac
venilson1 <venilson2009@hotmail.com>
2021-02-22 13:55:55 -0300

    routes definicion

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import express from 'express';
-
 const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({ message: "Hello Word - NLW#4" })
+});
+
+app.post('/', (req, res) => {
+  return res.json({ message: "Os dados foram salvos com sucesso - NLW#4" })
+});
+
+
 
 app.listen(3333, () => console.log('Server is Running, PORT 3333'));
