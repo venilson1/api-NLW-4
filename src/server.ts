5d69d58c955b314cca522241389f38a02100b248
venilson1 <venilson2009@hotmail.com>
2021-02-23 15:06:58 -0300

    routes & express.json

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routers';
 
 const app = express();
-
-
-
-app.get('/', (req, res) => {
-  return res.json({ message: "Hello Word - NLW#4" })
-});
-
-app.post('/', (req, res) => {
-  return res.json({ message: "Os dados foram salvos com sucesso - NLW#4" })
-});
-
-
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is Running, PORT 3333'));
