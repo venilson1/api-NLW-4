e95b626599b4552ba5f288faa6994329bdab6bc2
venilson1 <venilson2009@hotmail.com>
2021-02-24 12:10:13 -0300

    models

---
+++ src/models/Users.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+ 
+@Entity("users") //informa que é uma entidade| precisa habilitar os decorators
+class Users{
+
+  @PrimaryColumn() //chave primaria
+  readonly id: string;
+
+  @Column() //coluna  
+  name: string;
+  
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor(){ //caso não exista um id na criação, o id receberá o valor de um uuid
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Users }
