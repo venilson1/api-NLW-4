0f7f8bc3d00ddca1fd1bdaa07a85f19312158b4c
venilson1 <venilson2009@hotmail.com>
2021-02-25 21:48:12 -0300

    tdd with jest

---
+++ src/__tests__/First.test.ts
@@ -0,0 +1,10 @@
+describe("First", () => {
+
+  it("deve ser possivel somar 2 numeros", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it("deve ser possivel somar 2 numeros", () => {
+    expect(2 + 2).not.toBe(5);
+  });
+});
